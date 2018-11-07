50ebffeb1de97cd88301bd398f8337d393fe88d2
Dongwook Hwang <cytops1217@gmail.com>
2018-11-08 03:21:10 +0900

    solved CORS-related issue

--- src/index.js
+++ src/index.js
@@ -18,6 +18,28 @@ const router = new Router();
 router.use('/api', api.routes());
 
 const app = new Koa();
+
+/* configure CORS issue */
+app.use((ctx, next) => {
+    const whiteList = [
+        'localhost'
+    ];
+    const origin = ctx.header['origin'];
+    whiteList.every(el => {
+        if (!origin) return false;
+        if (origin.indexOf(el) !== -1) {
+            ctx.response.set('Access-Control-Allow-Origin', origin);
+            return false;
+        }
+        return true;
+    });
+    ctx.set('Access-Control-Allow-Credentials', true);
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-timebase, Link');
+    ctx.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS');
+    ctx.set('Access-Control-Expose-Headers', 'Link');
+    return next();
+});
+
 app.use(jwtMiddleware);
 app.use(bodyParser());
 app.use(router.routes());
